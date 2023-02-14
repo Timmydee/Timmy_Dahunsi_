2702de03d84e42f083aa00d37d5bec62c89631f4
Timmydee <dahunsitimmy@gmail.com>
2023-02-14 13:52:50 -0800

    work update

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -26,7 +26,7 @@ const allWork = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna pellentesque risus nec dictum cursus porttitor nisl. Sed laoreet.",
   },
   {
-    imgUrl: portvii,
+    imgUrl: portv,
     title: "Tradify",
     webUrl:"https://tradify-nine.vercel.app/",
     about:
