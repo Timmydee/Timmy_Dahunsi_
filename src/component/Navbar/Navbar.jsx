8ac58805b0bf3cbfc233726a5f373f9270277afd
Timmydee <dahunsitimmy@gmail.com>
2023-05-10 15:11:58 +0100

    navbar

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -24,19 +24,19 @@ const Navbar = () => {
         <div className={ click ? 'nav-menu active' : 'nav-menu'}>
           <ul className='nav'>
             <li>
-              <a href='#home'>Home</a>
+              <a onClick={() => setClick(false) } href='#home'>Home</a>
             </li>
 
             <li>
-              <a href='#about'>About</a>
+              <a onClick={() => setClick(false) } href='#about'>About</a>
             </li>
 
             <li>
-              <a href='#work'>Projects</a>
+              <a onClick={() => setClick(false) } href='#work'>Projects</a>
             </li>
 
             <li>
-              <a href='#contact'>Contact</a>
+              <a onClick={() => setClick(false) } href='#contact'>Contact</a>
             </li>
           </ul>
 
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -21,7 +21,13 @@ const About = () => {
           </div>
           <div className="about__text-wrapper">
             <p className="about__text">
-              Hi! I'm Dahunsi Timilehin, a frontend developer dedicated to creating stunning web and mobile apps. With 18 months' experience and product management expertise, I design user-focused digital solutions that drive growth. Skilled in React, JavaScript, HTML, CSS, React Native, and Next.js, I stay current with industry trends. My problem-solving and communication abilities ensure timely delivery and client satisfaction. Let's connect and bring your project to life with a touch of innovation
+              Hi! I'm Dahunsi Timilehin, a frontend developer dedicated to creating
+              stunning web and mobile apps. With 18 months' experience and product management
+              expertise, I design user-focused digital solutions that drive growth. 
+              Skilled in React, JavaScript, HTML, CSS, React Native, and Next.js,
+               I stay current with industry trends. My problem-solving and communication
+                abilities ensure timely delivery and client satisfaction. Let's connect and
+                 bring your project to life with a touch of innovation
             </p>
           </div>
 
