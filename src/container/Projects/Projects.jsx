093c4b8b878b4e44e66e79a32f5fb9f38bf83d52
Timmydee <dahunsitimmy@gmail.com>
2023-09-29 05:43:45 -0400

    port

--- src/container/Projects/Projects.jsx
+++ src/container/Projects/Projects.jsx
@@ -37,13 +37,27 @@ const allWork = [
       about:
         "I developed the frontend of a this web application that serves as a portfolio website for my client.",
     },
+    // {
+    //   imgUrl: port1,
+    //   title: "Best Crib",
+    //   webUrl:"https://bestcrib.vercel.app/",
+    //   about:
+    //     "Best Crib is a housing solution that helps home seekers find vacant apartments in my Local community.",
+    // },
     {
-      imgUrl: port1,
-      title: "Best Crib",
-      webUrl:"https://bestcrib.vercel.app/",
-      about:
-        "Best Crib is a housing solution that helps home seekers find vacant apartments in my Local community.",
-    },
+        imgUrl: port1,//
+        title: "Xypher",
+        webUrl:"https://xyzpher.vercel.app/",
+        about:
+          'I actively collaborated with designers to build the frontend of Xypher, a decentralized investment platform, utilizing Next.js and Tailwind CSS to create a seamless and user-friendly interface.',
+      },
+      {
+        imgUrl: port1,
+        title: "Krane Finance",
+        webUrl:"https://krane-fe.vercel.app/",
+        about:
+          "As a frontend developer in an agile team, I am actively contributing to the ongoing development of Krane Finance, an innovative platform aimed at simplifying car loan processes. My focus is on crafting an intuitive and visually engaging user interface to enhance the borrowing experience for individuals as the project evolves.",
+      },
     {
       imgUrl: portvii,
       title: "Tradify",
